1ebe8158326f8febe6ede3703720e56b8d221580
KWi <s34102@pjwstk.edu.pl>
2026-06-20 16:46:25 +0200

    Added final changes to the code, fixed bugs

--- src/main/java/com/example/zaliczenie/ZaliczenieApplication.java
+++ src/main/java/com/example/zaliczenie/ZaliczenieApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZaliczenieApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZaliczenieApplication.class, args);
 	}
--- src/main/java/com/example/zaliczenie/exception/NbpApiException.java
+++ src/main/java/com/example/zaliczenie/exception/NbpApiException.java
@@ -1,6 +1,5 @@
 package com.example.zaliczenie.exception;
 
-// tym raem gdy NBP ma inny błąd (400, 500 i tego typu)
 public class NbpApiException extends RuntimeException {
 
     private final int statusCode;
@@ -14,4 +13,3 @@ public class NbpApiException extends RuntimeException {
         return statusCode;
     }
 }
-
